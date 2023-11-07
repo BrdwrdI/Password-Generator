ebf5db3902cdec27c74f5babab042355d672fbe9
WRD <dandobilly@gmail.com>
2023-11-07 01:34:29 -0500

    started for loop worked on adding more to if statements

--- script.js
+++ script.js
@@ -12,27 +12,30 @@ function writePassword() {
 
 function generatePassword() {
   var password = 0;
-  
+  var lowercaseChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var uppercaseChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var specCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '{', '}', '[', ']', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/', '|', '\\'];
+  var numCharacters = ['0','1','2','3','4','5','6','7','8','9'];
+
   //prompt for length
 
   var passLength = prompt("Choose a length for your password between 8 and 128 chararcters");
 
-  if (passLength > 8 && passLength < 128){
+  if (passLength >= 8 && passLength <= 128){
     ;
   } else {
-
-    return;
+    return generatePassword();
   }
 
   //prompt for lowercase
 
-var lowerCase = prompt("Would you like Lowercase characters; Yes , No")
+  var lowerCase = prompt("Would you like Lowercase characters; Yes , No")
 
-if (lowerCase === "Yes"){
-  ;
-} else {
-  ;
-}
+  if (lowerCase === "Yes"){
+    ;
+  } else {
+    ;
+  }
 
   //promt for uppercase 
 
@@ -63,6 +66,16 @@ if (lowerCase === "Yes"){
   } else {
     ;
   }
+
+  //for loop to interate through the max number of characters 
+  //randomly assigning specChars/numChars/upperCase/lowerCase
+  //
+
+  for (var i = 0; i < passLength; i++) {
+    
+  }
+
+
 
   return password;
 }
